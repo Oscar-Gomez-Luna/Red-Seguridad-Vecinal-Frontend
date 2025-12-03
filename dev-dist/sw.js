a5889f6af32b001aa6ef9fb14b187f99830f25ea
Oscar-Gomez-Luna <oscardio.lunagomez@gmail.com>
2025-12-03 12:04:23 -0600

    Cambios del sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.t0stqprs5bo"
+    "revision": "0.756hc8k0438"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
